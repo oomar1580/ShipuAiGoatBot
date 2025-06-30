eeaea262e4f4a1fbd13e412427317d6cbd6ebcbb
goat-bot-v <kakashib28@gmail.com>
2025-06-30 12:14:31 +0500

    Update advice.js

--- scripts/cmds/advice.js
+++ scripts/cmds/advice.js
@@ -3,7 +3,7 @@ module.exports = {
   config: {
     name: "advice",
     version: "1.0",
-    author: "Chitron Bhattacharjee",
+    author: "opu",
     countDown: 5,
     role: 0,
     shortDescription: { en: "Random advice" },
