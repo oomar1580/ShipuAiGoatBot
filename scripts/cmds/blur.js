de9ced2fb44db1f0be6cdb1fbc827917ea0fc66c
goat-bot-v <kakashib28@gmail.com>
2025-06-30 16:55:57 +0500

    Update blur.js

--- scripts/cmds/blur.js
+++ scripts/cmds/blur.js
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     name: "blur",
     version: "1.0",
-    author: "Chitron Bhattacharjee",
+    author: "opu",
     countDown: 10,
     role: 0,
     shortDescription: {
