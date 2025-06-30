f078ac729eb857051c87a180e21b2fc7c13d45a7
goat-bot-v <kakashib28@gmail.com>
2025-06-30 20:17:23 +0500

    Update pet.js

--- scripts/cmds/pet.js
+++ scripts/cmds/pet.js
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     name: "pet",
     version: "1.0",
-    author: "Chitron Bhattacharjee",
+    author: "opu",
     countDown: 10,
     role: 0,
     shortDescription: {
