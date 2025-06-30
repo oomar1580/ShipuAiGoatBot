cb09089692962563faa6300db14109964519b6a9
goat-bot-v <kakashib28@gmail.com>
2025-06-30 12:43:09 +0500

    Update bacon.js

--- scripts/cmds/bacon.js
+++ scripts/cmds/bacon.js
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     name: "bacon",
     version: "1.0",
-    author: "Chitron Bhattacharjee",
+    author: "opu",
     countDown: 5,
     role: 0,
     shortDescription: { en: "Random bacon image" },
