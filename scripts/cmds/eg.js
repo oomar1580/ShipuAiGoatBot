ba25dc4839d08a74e5b46cf11c0c8d3bf4c547ed
goat-bot-v <kakashib28@gmail.com>
2025-06-30 17:48:05 +0500

    Update eg.js

--- scripts/cmds/eg.js
+++ scripts/cmds/eg.js
@@ -9,7 +9,7 @@ module.exports = {
 	config: {
 		name: "commandName", // Name of command, it must be unique to identify with other commands
 		version: "1.1", // Version of command
-		author: "Chitron Bhattacharjee", // Author name must not be changed
+		author: "opu", // Author name must not be changed
 		countDown: 5, // Time to wait before executing command again (seconds)
 		role: 0, // Role of user to use this command (0: normal user, 1: admin box chat, 2: owner bot)
 		shortDescription: {
