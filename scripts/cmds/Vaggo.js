4bfb8a0ce61bb0f7f132ca2fb21beca78e15ceff
goat-bot-v <kakashib28@gmail.com>
2025-06-30 11:38:07 +0500

    Update Vaggo.js

--- scripts/cmds/Vaggo.js
+++ scripts/cmds/Vaggo.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "vaggo",
     aliases: ["bhobisshot", "fate"],
     version: "2.0",
-    author: "Chitron Bhattacharjee",
+    author: "OPU SENSE",
     role: 0,
     category: "fun",
     shortDescription: {
