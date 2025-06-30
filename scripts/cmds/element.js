e9c501ae0528b197ec7ea4405af4b884a49831cb
goat-bot-v <kakashib28@gmail.com>
2025-06-30 17:50:04 +0500

    Update element.js

--- scripts/cmds/element.js
+++ scripts/cmds/element.js
@@ -4,7 +4,7 @@ module.exports = {
   config: {
     name: "element",
     version: "1.0",
-    author: "Chitron Bhattacharjee",
+    author: "opu",
     countDown: 5,
     role: 0,
     shortDescription: {
